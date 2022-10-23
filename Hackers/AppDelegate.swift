ca38d8e3760ce0edb6129ad10199188ae3be1af8
Kyle Beard <kylebeard@picasso.attlocal.net>
2022-10-23 17:02:35 -0400

    initial commit

---
+++ Hackers/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 10/23/22.
+//
+
+import Foundation
---
+++ Hackers/HackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackersApp.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct HackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
