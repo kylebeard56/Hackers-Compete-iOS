42c3a3015c85e299efc066787e95ea125bf56ff3
Kyle Beard <kylebeard56@gmail.com>
2024-06-12 13:56:21 -0400

    tab view, scorecard shapes, hole navigation

---
+++ Hackers/UI/Hole/HoleView2.swift
@@ -0,0 +1,18 @@
+//
+//  HoleView2.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 6/7/24.
+//
+
+import SwiftUI
+
+struct HoleView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HoleView2()
+}
---
+++ Hackers/UI/Hole/LeaderboardView.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderboardView.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 6/11/24.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeaderboardView()
+}
