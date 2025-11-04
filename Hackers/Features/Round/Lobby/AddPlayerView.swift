c3bdd93e9926a4f3b53dff2de4936ecce9d07199
Kyle Beard <kylebeard56@gmail.com>
2025-11-04 07:22:38 -0500

    add players to lobby

---
+++ Hackers/Features/Round/Lobby/AddPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPlayerView.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 11/3/25.
+//
+
+import SwiftUI
+
+struct AddPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPlayerView()
+}
---
+++ Hackers/Features/Round/Lobby/ShareRoundView.swift
@@ -0,0 +1,8 @@
+//
+//  ShareRoundView.swift
+//  Hackers
+//
+//  Created by Kyle Beard on 11/2/25.
+//
+
+import Foundation
